b24dde1e0473b948b61e9fdfc7f062d3a27e9818
VICTUS <hkhanhly6125@gmail.com>
2026-04-28 11:15:05 +0700

    Add OrderHistory tests and page objects

--- src/main/java/common/Constant.java
+++ src/main/java/common/Constant.java
@@ -11,4 +11,8 @@ public class Constant {
     public static final String URL = "http://localhost:8080/";
     public static final String USERNAME = "phuongthao@gmail.com";
     public static final String PASSWORD = "123456";
+    public static final String EMPTY_USERNAME = "empty@gmail.com";
+    public static final String EMPTY_PASSWORD = "123456";
+    public static final String NO_ORDER_USERNAME = "khanhly@gmail.com";
+    public static final String NO_ORDER_PASSWORD = "123456";
 }
